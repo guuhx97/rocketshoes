423f08e1618352a040b4de77358deb70ef8af7ee
guuhx97 <gustavorobertotb@outlook.com>
2020-03-20 22:23:39 -0300

    :memo: Feat: Adiciona arquivo README e LICENSE no projeto

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -43,7 +43,40 @@ export default function Cart() {
               </div>
             </td>
             <td>
-              <strong>R$258,80</strong>
+              <strong>R$129,90</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-feminino/15/HZM-1729-715/HZM-1729-715_detalhe1.jpg?ts=1583429518?ims=280x280"
+                alt="Tenis"
+              />
+            </td>
+            <td>
+              <strong>Tênis muito massa</strong>
+              <span>R$229,90</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={1} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$229,90</strong>
             </td>
             <td>
               <button type="button">
@@ -59,7 +92,7 @@ export default function Cart() {
 
         <Total>
           <span>TOTAL</span>
-          <strong>R$1920,28</strong>
+          <strong>R$359,80</strong>
         </Total>
       </footer>
     </Container>
