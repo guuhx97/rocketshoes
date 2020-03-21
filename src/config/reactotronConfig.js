c80ecc3853f8d56b5e77f57c6c59f5b7fd1f49f6
guuhx97 <gustavorobertotb@outlook.com>
2020-03-21 13:46:53 -0300

    :rocket: Fix: Adicionado REDUX a aplicação para utilizar o state global

---
+++ src/config/reactotronConfig.js
@@ -0,0 +1,10 @@
+import Reactotron from 'reactotron-react-js';
+import { reactotronRedux } from 'reactotron-redux';
+
+const tron = Reactotron.configure()
+  .use(reactotronRedux())
+  .connect();
+
+tron.clear();
+
+console.tron = tron;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import { MdAddShoppingCart } from 'react-icons/md';
 import { formatPrice } from '../../util/format'
+
+import * as CartActions from '../../store/modules/cart/actions';
 
 import api from '../../services/api';
 
 import { ProductList } from './styles';
 
 // eslint-disable-next-line react/prefer-stateless-function
-export default class Home extends Component {
+ class Home extends Component {
   state = {
     products: []
   };
@@ -22,8 +26,15 @@ export default class Home extends Component {
     this.setState({ products: data});
   }
 
+  handleAddProduct = product => {
+    const { addToCart } = this.props;
+
+    addToCart(product);
+  }
+
   render() {
     const { products } = this.state;
+    const { amount } = this.props;
 
     return (
       <ProductList>
@@ -36,11 +47,10 @@ export default class Home extends Component {
             <strong>{product.title}</strong>
             <span>{product.priceFormatted}</span>
 
-            <button type="button">
+            <button type="button" onClick={() => this.handleAddProduct(product)}>
               <div>
-                <MdAddShoppingCart size={16} color="#FFF" />3
+                <MdAddShoppingCart size={16} color="#FFF" />{amount[product.id]  || 0}
               </div>
-
               <span>ADICIONAR AO CARRINHO</span>
             </button>
           </li>
@@ -50,3 +60,16 @@ export default class Home extends Component {
     );
   }
 }
+
+const mapStateToProps = state => ({
+  amount: state.cart.reduce((amount, product) => {
+    amount[product.id] = product.amount;
+
+    return amount
+  }, {}),
+});
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(CartActions, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
